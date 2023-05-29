c5341e2efe37c18013854935bb264013409398b9
Marko <marko.separovic@qubinets.com>
2023-05-30 00:55:43 +0200

    Added routing to gui

--- volcano-gui/src/app/app-routing.module.ts
+++ volcano-gui/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { RegisterActivityComponent } from './components/register-activity/register-activity.component';
+import { ActivityLogComponent } from './components/activity-log/activity-log.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'register-activity', component: RegisterActivityComponent },
+  { path: 'activity-log', component: ActivityLogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ volcano-gui/src/app/components/register-activity/register-activity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-activity',
+  templateUrl: './register-activity.component.html',
+  styleUrls: ['./register-activity.component.css']
+})
+export class RegisterActivityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
